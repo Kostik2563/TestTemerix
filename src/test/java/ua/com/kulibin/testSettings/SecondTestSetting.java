5a48a7b62649b4c22bf25f405b87531acee791c8
Kostyantin <77070622+Kostik2563@users.noreply.github.com>
2021-07-25 15:50:36 +0300

    this project was performed by Shcherban Kostiantyn

--- src/test/java/ua/com/kulibin/testSettings/SecondTestSetting.java
+++
@@ -1,9 +0,0 @@
-package ua.com.kulibin.testSettings;
-
-public interface SecondTestSetting {
-    // колиество страниц которые будут проверяться на наличие цен
-    int NUMBER_TEST_PAGES = 2;
-
-    // если поставить true, то, в случаи если товар не имеет цену, на экран будет выводиться ссылка на этот товар
-    boolean RECEIVE_INFORMATION_ABOUT_PRODUCT_WHERE_NO_PRICE = false;
-}
